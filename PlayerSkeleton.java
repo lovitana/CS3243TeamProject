c9f9cb73bcdeefb7bff4aa9dec825a46346ec1c5
Loïc Vandenberghe <loic.vandenberghe@epfl.ch>
2018-05-07 19:02:31 +0800

    fixed version that does not use State

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -1,5 +1,6 @@
 
 
+import java.awt.Color;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -47,9 +48,9 @@ public class PlayerSkeleton {
 		 *            the state of the game
 		 * @return number of filled tiles above every holes
 		 */
-		public static int squaresAboveHoles(State s) {
+		public static int squaresAboveHoles(CopyState s) {
 			int count = 0;
-			for (int c = 0; c < State.COLS; c++) {
+			for (int c = 0; c < CopyState.COLS; c++) {
 				int end = s.getTop()[c];
 				for (int r = 0; r < end; r++) {
 					boolean isHole = s.getField()[r][c] == 0;
@@ -69,21 +70,21 @@ public class PlayerSkeleton {
 		 *            the state of the game
 		 * @return number of grouped holes.
 		 */
-		public static int groupedHoles(State s) {
+		public static int groupedHoles(CopyState s) {
 			int[][] grid = s.getField().clone();
 			// fill up all non-holes
 
 			// for each col, set all squares above to 1
-			for (int c = 0; c < State.COLS; c++) {
+			for (int c = 0; c < CopyState.COLS; c++) {
 				int start = s.getTop()[c];
-				for (int r = start; r < State.ROWS; r++) {
+				for (int r = start; r < CopyState.ROWS; r++) {
 					grid[r][c] = 1;
 				}
 			}
 
 			// count number of hole groups (all connected holes make up 1 group)
 			int numGroups = 0;
-			for (int c = 0; c < State.COLS; c++) {
+			for (int c = 0; c < CopyState.COLS; c++) {
 				for (int r = 0; r < s.getTop()[c]; r++) {
 					boolean isNotHole = grid[r][c] > 0;
 					if (isNotHole)
@@ -132,9 +133,9 @@ public class PlayerSkeleton {
 		 *            the state of the game
 		 * @return the sum
 		 */
-		public static int sumOfHeights(State s) {
+		public static int sumOfHeights(CopyState s) {
 			int sum = 0;
-			for (int i = 0; i < State.COLS; i++) {
+			for (int i = 0; i < CopyState.COLS; i++) {
 				sum += s.getTop()[i];
 			}
 			return sum;
@@ -147,7 +148,7 @@ public class PlayerSkeleton {
 		 *            the state of the game
 		 * @return
 		 */
-		public static int maxHeightsDifference(State s) {
+		public static int maxHeightsDifference(CopyState s) {
 			Integer[] box = new Integer[s.getTop().length];
 			for (int i = 0; i < box.length; i++) {
 				box[i] = s.getTop()[i];
@@ -158,9 +159,9 @@ public class PlayerSkeleton {
 			return (highest - lowest);
 		}
 
-		public static int holes(State s) {
+		public static int holes(CopyState s) {
 			int holes = 0;
-			for (int i = 0; i < State.COLS; i++) {
+			for (int i = 0; i < CopyState.COLS; i++) {
 				int height = s.getTop()[i];
 				for (int j = 0; j < height - 1; j++) {
 					if (s.getField()[j][i] == 0) {
@@ -188,7 +189,7 @@ public class PlayerSkeleton {
 		 *            the weights parameters of the heuristic
 		 * @return the heuristic result
 		 */
-		public float compute(State next, float[] w);
+		public float compute(CopyState next, float[] w);
 
 		/**
 		 * Gives an approximation of the current state based on the heuristic
@@ -197,7 +198,7 @@ public class PlayerSkeleton {
 		 *            The state to approximate
 		 * @return The vector of feature values at the state
 		 */
-		public float[] featureValues(State state); // FIXME ????
+		public float[] featureValues(CopyState state); // FIXME ????
 
 		/**
 		 * @return the number of weight used by the solver
@@ -212,15 +213,15 @@ public class PlayerSkeleton {
 	 */
 	public static class GivenHeuristic implements Heuristic {
 
-		private static final int LENGTH = State.COLS + State.COLS - 1 + 3;
+		private static final int LENGTH = CopyState.COLS + CopyState.COLS - 1 + 3;
 
 		private static final int INDICE_COLS_WEIGHTS = 1;
-		private static final int INDICE_COLS_DIFF_WEIGTHS = INDICE_COLS_WEIGHTS + State.COLS;
-		private static final int INDICE_MAX_HEIGHT_WEIGHT = INDICE_COLS_DIFF_WEIGTHS + State.COLS - 1;
+		private static final int INDICE_COLS_DIFF_WEIGTHS = INDICE_COLS_WEIGHTS + CopyState.COLS;
+		private static final int INDICE_MAX_HEIGHT_WEIGHT = INDICE_COLS_DIFF_WEIGTHS + CopyState.COLS - 1;
 		private static final int INDICE_HOLES_WEIGHT = INDICE_MAX_HEIGHT_WEIGHT + 1;
 
 		@Override
-		public float compute(State state, float[] weights) {
+		public float compute(CopyState state, float[] weights) {
 
 			if (state.lost) {
 				return Float.NEGATIVE_INFINITY;
@@ -229,7 +230,7 @@ public class PlayerSkeleton {
 			float heuristicValue = weights[0];
 			int maxHeight = 0;
 
-			for (int i = 0; i < State.COLS; i++) {
+			for (int i = 0; i < CopyState.COLS; i++) {
 				// height
 				int height = state.getTop()[i];
 				heuristicValue += weights[INDICE_COLS_WEIGHTS + i] * height;
@@ -243,7 +244,7 @@ public class PlayerSkeleton {
 			heuristicValue += HeuristicTool.holes(state) * weights[INDICE_HOLES_WEIGHT];
 
 			// differences
-			for (int i = 0; i < State.COLS - 1; i++) {
+			for (int i = 0; i < CopyState.COLS - 1; i++) {
 				int diff = Math.abs(state.getTop()[i] - state.getTop()[i + 1]);
 
 				heuristicValue += weights[INDICE_COLS_DIFF_WEIGTHS + i] * diff;
@@ -258,11 +259,11 @@ public class PlayerSkeleton {
 		}
 
 		@Override
-		public float[] featureValues(State state) {
+		public float[] featureValues(CopyState state) {
 			float[] values = new float[LENGTH];
 			int maxHeight = 0;
 			int holes = 0;
-			for (int i = 0; i < State.COLS; i++) {
+			for (int i = 0; i < CopyState.COLS; i++) {
 				// height
 				int height = state.getTop()[i];
 				values[i] = height;
@@ -281,9 +282,9 @@ public class PlayerSkeleton {
 			values[LENGTH - 2] = maxHeight;
 
 			// differences
-			for (int i = 0; i < State.COLS - 1; i++) {
+			for (int i = 0; i < CopyState.COLS - 1; i++) {
 				int diff = Math.abs(state.getTop()[i] - state.getTop()[i + 1]);
-				values[State.COLS + i] = diff;
+				values[CopyState.COLS + i] = diff;
 			}
 			return values;
 		}
@@ -296,7 +297,7 @@ public class PlayerSkeleton {
 	public static class ImprovedHeuristics extends GivenHeuristic {
 
 		@Override
-		public float compute(State next, float[] w) {
+		public float compute(CopyState next, float[] w) {
 			float oldScore = super.compute(next, w);
 
 			int nextIndex = super.weightsLength();
@@ -330,7 +331,7 @@ public class PlayerSkeleton {
 		}
 
 		@Override
-		public float[] featureValues(State state) {
+		public float[] featureValues(CopyState state) {
 			float[] oldValues = super.featureValues(state);
 
 			float[] newValues = new float[oldValues.length + weightsLength()];
@@ -377,7 +378,7 @@ public class PlayerSkeleton {
 		 *            The state to approximate
 		 * @return The feature vector of the state
 		 */
-		public float[] featureValues(State s);
+		public float[] featureValues(CopyState s);
 
 		/**
 		 * @return the number of weight used by the solver
@@ -395,18 +396,41 @@ public class PlayerSkeleton {
 		 *            move played
 		 * @return the state of the game after playing the selected move
 		 */
-		public static State nextState(State s, int[] move) {
-			State next = new State();
+		public static CopyState copy(State s) {
+			CopyState copy = new CopyState();
+			copy.lost = s.lost;
+			copy.nextPiece = s.nextPiece;
+			int[][] field = s.getField();
+			int[][] copyField = copy.getField();
+			for (int i = 0; i < CopyState.ROWS; i++) {
+				for (int j = 0; j < CopyState.COLS; j++) {
+					copyField[i][j] = field[i][j];
+				}
+			}
+			for (int i = 0; i < CopyState.COLS; i++) {
+				copy.getTop()[i] = s.getTop()[i];
+			}
+			return copy;
+		}
+		public static CopyState nextState(State s, int[] move) {
+			CopyState next = copy(s);
+
+			next.makeMove(move);
+
+			return next;
+		}
+		public static CopyState nextState(CopyState s, int[] move) {
+			CopyState next = new CopyState();
 			next.lost = s.lost;
 			next.nextPiece = s.nextPiece;
 			int[][] field = s.getField();
 			int[][] copyField = next.getField();
-			for (int i = 0; i < State.ROWS; i++) {
-				for (int j = 0; j < State.COLS; j++) {
+			for (int i = 0; i < CopyState.ROWS; i++) {
+				for (int j = 0; j < CopyState.COLS; j++) {
 					copyField[i][j] = field[i][j];
 				}
 			}
-			for (int i = 0; i < State.COLS; i++) {
+			for (int i = 0; i < CopyState.COLS; i++) {
 				next.getTop()[i] = s.getTop()[i];
 			}
 
@@ -441,7 +465,8 @@ public class PlayerSkeleton {
 				throw new IllegalArgumentException(
 						"wrong number of weights: " + w.length + ". Expected: " + weightsLength());
 			}
-			List<Float> values = Arrays.stream(legalMoves).parallel().map(move -> getHeuristicValue(move, s, w))
+			CopyState copy = TetrisSolver.copy(s);
+			List<Float> values = Arrays.stream(legalMoves).parallel().map(move -> getHeuristicValue(move, copy, w))
 					.collect(Collectors.toList());
 
 			statesEvaluated += values.size();
@@ -451,8 +476,8 @@ public class PlayerSkeleton {
 			return maxIdx;
 		}
 
-		private float getHeuristicValue(int[] move, State state, float[] weights) {
-			State next = TetrisSolver.nextState(state, move);
+		private float getHeuristicValue(int[] move, CopyState state, float[] weights) {
+			CopyState next = TetrisSolver.nextState(state, move);
 			return heuristic.compute(next, weights);
 		}
 
@@ -461,7 +486,7 @@ public class PlayerSkeleton {
 			return heuristic.weightsLength();
 		}
 
-		public float[] featureValues(State s) {
+		public float[] featureValues(CopyState s) {
 			return heuristic.featureValues(s);
 		}
 
@@ -499,7 +524,7 @@ public class PlayerSkeleton {
 			while (max == Float.NEGATIVE_INFINITY && d >= 0) {
 				int n = 0;
 				for (int[] move : legalMoves) {
-					State next = TetrisSolver.nextState(s, move);
+					CopyState next = TetrisSolver.nextState(s, move);
 					float heuristicValue = minmax(next, d, false, weights, max, Float.POSITIVE_INFINITY);
 
 					if (heuristicValue > max) {
@@ -533,7 +558,7 @@ public class PlayerSkeleton {
 		 *            weights
 		 * @return the MINMAX best heuristic value
 		 */
-		private float minmax(State s, int d, boolean maximizing, float[] weights, float alpha, float beta) {
+		private float minmax(CopyState s, int d, boolean maximizing, float[] weights, float alpha, float beta) {
 			if (s.hasLost()) {
 				return Float.NEGATIVE_INFINITY;
 			}
@@ -544,7 +569,7 @@ public class PlayerSkeleton {
 			if (maximizing) {
 				float best = Float.NEGATIVE_INFINITY;
 				for (int[] move : s.legalMoves()) {
-					State next = TetrisSolver.nextState(s, move);
+					CopyState next = TetrisSolver.nextState(s, move);
 					float v = minmax(next, d - 1, false, weights, alpha, beta);
 					if (v > best) {
 						best = v;
@@ -559,7 +584,7 @@ public class PlayerSkeleton {
 				return best;
 			} else {
 				float best = Float.POSITIVE_INFINITY;
-				for (int i = 0; i < State.N_PIECES; i++) {
+				for (int i = 0; i < CopyState.N_PIECES; i++) {
 					s.nextPiece = i;
 					float v = minmax(s, d, true, weights, alpha, beta);
 					if (v < best) {
@@ -576,7 +601,7 @@ public class PlayerSkeleton {
 			}
 		}
 
-		public float[] featureValues(State s) {
+		public float[] featureValues(CopyState s) {
 			return heuristic.featureValues(s);
 		}
 	}
@@ -601,15 +626,353 @@ public class PlayerSkeleton {
 		State s = new State();
 		new TFrame(s);
 		TetrisSolver aI = MINMAX_SOLVER;
+		long startTime = System.currentTimeMillis();
+		int i = 0;
 		while (!s.hasLost()) {
 			s.makeMove(aI.pickMove(s, s.legalMoves(), BEST_WEIGHTS));
 			 s.draw();
 			 s.drawNext(0,0);
-		
-			try { Thread.sleep(100); } catch (InterruptedException e) {
-				 e.printStackTrace(); }
+			 if(i%1_000 ==0) {
+				 System.out.println("Played "+ i+" pieces in\n"
+				 		+ ((System.currentTimeMillis()-startTime)/1000) + " Seconds");
+			 }
+			 i++;
+			 
+			//try { Thread.sleep(100); } catch (InterruptedException e) {
+				// e.printStackTrace(); }
 		}
 		System.out.println("You have completed " + s.getRowsCleared() + " rows.");
 	}
 
+
+
+
+
+
+	/**
+	 * Copy of State class.
+	 * Permit to use a state without depending on the given State class
+	 *
+	 */
+	public static class CopyState {
+		public static final int COLS = 10;
+		public static final int ROWS = 21;
+		public static final int N_PIECES = 7;
+
+		
+
+		public boolean lost = false;
+		
+		
+		
+
+		
+		public TLabel label;
+		
+		//current turn
+		private int turn = 0;
+		private int cleared = 0;
+		
+		//each square in the grid - int means empty - other values mean the turn it was placed
+		private int[][] field = new int[ROWS][COLS];
+		//top row+1 of each column
+		//0 means empty
+		private int[] top = new int[COLS];
+		
+		
+		//number of next piece
+		protected int nextPiece;
+		
+		
+		
+		//all legal moves - first index is piece type - then a list of 2-length arrays
+		protected static int[][][] legalMoves = new int[N_PIECES][][];
+		
+		//indices for legalMoves
+		public static final int ORIENT = 0;
+		public static final int SLOT = 1;
+		
+		//possible orientations for a given piece type
+		protected static int[] pOrients = {1,2,4,4,4,2,2};
+		
+		//the next several arrays define the piece vocabulary in detail
+		//width of the pieces [piece ID][orientation]
+		protected static int[][] pWidth = {
+				{2},
+				{1,4},
+				{2,3,2,3},
+				{2,3,2,3},
+				{2,3,2,3},
+				{3,2},
+				{3,2}
+		};
+		//height of the pieces [piece ID][orientation]
+		private static int[][] pHeight = {
+				{2},
+				{4,1},
+				{3,2,3,2},
+				{3,2,3,2},
+				{3,2,3,2},
+				{2,3},
+				{2,3}
+		};
+		private static int[][][] pBottom = {
+			{{0,0}},
+			{{0},{0,0,0,0}},
+			{{0,0},{0,1,1},{2,0},{0,0,0}},
+			{{0,0},{0,0,0},{0,2},{1,1,0}},
+			{{0,1},{1,0,1},{1,0},{0,0,0}},
+			{{0,0,1},{1,0}},
+			{{1,0,0},{0,1}}
+		};
+		private static int[][][] pTop = {
+			{{2,2}},
+			{{4},{1,1,1,1}},
+			{{3,1},{2,2,2},{3,3},{1,1,2}},
+			{{1,3},{2,1,1},{3,3},{2,2,2}},
+			{{3,2},{2,2,2},{2,3},{1,2,1}},
+			{{1,2,2},{3,2}},
+			{{2,2,1},{2,3}}
+		};
+		
+		//initialize legalMoves
+		{
+			//for each piece type
+			for(int i = 0; i < N_PIECES; i++) {
+				//figure number of legal moves
+				int n = 0;
+				for(int j = 0; j < pOrients[i]; j++) {
+					//number of locations in this orientation
+					n += COLS+1-pWidth[i][j];
+				}
+				//allocate space
+				legalMoves[i] = new int[n][2];
+				//for each orientation
+				n = 0;
+				for(int j = 0; j < pOrients[i]; j++) {
+					//for each slot
+					for(int k = 0; k < COLS+1-pWidth[i][j];k++) {
+						legalMoves[i][n][ORIENT] = j;
+						legalMoves[i][n][SLOT] = k;
+						n++;
+					}
+				}
+			}
+		
+		}
+		
+		
+		public int[][] getField() {
+			return field;
+		}
+
+		public int[] getTop() {
+			return top;
+		}
+
+	    public int[] getpOrients() {
+	        return pOrients;
+	    }
+	    
+	    public  int[][] getpWidth() {
+	        return pWidth;
+	    }
+
+	    public  int[][] getpHeight() {
+	        return pHeight;
+	    }
+
+	    public  int[][][] getpBottom() {
+	        return pBottom;
+	    }
+
+	    public  int[][][] getpTop() {
+	        return pTop;
+	    }
+
+
+		public int getNextPiece() {
+			return nextPiece;
+		}
+		
+		public boolean hasLost() {
+			return lost;
+		}
+		
+		public int getRowsCleared() {
+			return cleared;
+		}
+		
+		public int getTurnNumber() {
+			return turn;
+		}
+		
+		
+		
+		//constructor
+		public CopyState() {
+			nextPiece = randomPiece();
+
+		}
+		
+		//random integer, returns 0-6
+		private int randomPiece() {
+			return (int)(Math.random()*N_PIECES);
+		}
+		
+
+
+		
+		//gives legal moves for 
+		public int[][] legalMoves() {
+			return legalMoves[nextPiece];
+		}
+		
+		//make a move based on the move index - its order in the legalMoves list
+		public void makeMove(int move) {
+			makeMove(legalMoves[nextPiece][move]);
+		}
+		
+		//make a move based on an array of orient and slot
+		public void makeMove(int[] move) {
+			makeMove(move[ORIENT],move[SLOT]);
+		}
+		
+		//returns false if you lose - true otherwise
+		public boolean makeMove(int orient, int slot) {
+			turn++;
+			//height if the first column makes contact
+			int height = top[slot]-pBottom[nextPiece][orient][0];
+			//for each column beyond the first in the piece
+			for(int c = 1; c < pWidth[nextPiece][orient];c++) {
+				height = Math.max(height,top[slot+c]-pBottom[nextPiece][orient][c]);
+			}
+			
+			//check if game ended
+			if(height+pHeight[nextPiece][orient] >= ROWS) {
+				lost = true;
+				return false;
+			}
+
+			
+			//for each column in the piece - fill in the appropriate blocks
+			for(int i = 0; i < pWidth[nextPiece][orient]; i++) {
+				
+				//from bottom to top of brick
+				for(int h = height+pBottom[nextPiece][orient][i]; h < height+pTop[nextPiece][orient][i]; h++) {
+					field[h][i+slot] = turn;
+				}
+			}
+			
+			//adjust top
+			for(int c = 0; c < pWidth[nextPiece][orient]; c++) {
+				top[slot+c]=height+pTop[nextPiece][orient][c];
+			}
+			
+			int rowsCleared = 0;
+			
+			//check for full rows - starting at the top
+			for(int r = height+pHeight[nextPiece][orient]-1; r >= height; r--) {
+				//check all columns in the row
+				boolean full = true;
+				for(int c = 0; c < COLS; c++) {
+					if(field[r][c] == 0) {
+						full = false;
+						break;
+					}
+				}
+				//if the row was full - remove it and slide above stuff down
+				if(full) {
+					rowsCleared++;
+					cleared++;
+					//for each column
+					for(int c = 0; c < COLS; c++) {
+
+						//slide down all bricks
+						for(int i = r; i < top[c]; i++) {
+							field[i][c] = field[i+1][c];
+						}
+						//lower the top
+						top[c]--;
+						while(top[c]>=1 && field[top[c]-1][c]==0)	top[c]--;
+					}
+				}
+			}
+		
+
+			//pick a new piece
+			nextPiece = randomPiece();
+			
+
+			
+			return true;
+		}
+		
+		public void draw() {
+			label.clear();
+			label.setPenRadius();
+			//outline board
+			label.line(0, 0, 0, ROWS+5);
+			label.line(COLS, 0, COLS, ROWS+5);
+			label.line(0, 0, COLS, 0);
+			label.line(0, ROWS-1, COLS, ROWS-1);
+			
+			//show bricks
+					
+			for(int c = 0; c < COLS; c++) {
+				for(int r = 0; r < top[c]; r++) {
+					if(field[r][c] != 0) {
+						drawBrick(c,r);
+					}
+				}
+			}
+			
+			for(int i = 0; i < COLS; i++) {
+				label.setPenColor(Color.red);
+				label.line(i, top[i], i+1, top[i]);
+				label.setPenColor();
+			}
+			
+			label.show();
+			
+			
+		}
+		
+		public static final Color brickCol = Color.gray; 
+		
+		private void drawBrick(int c, int r) {
+			label.filledRectangleLL(c, r, 1, 1, brickCol);
+			label.rectangleLL(c, r, 1, 1);
+		}
+		
+		public void drawNext(int slot, int orient) {
+			for(int i = 0; i < pWidth[nextPiece][orient]; i++) {
+				for(int j = pBottom[nextPiece][orient][i]; j <pTop[nextPiece][orient][i]; j++) {
+					drawBrick(i+slot, j+ROWS+1);
+				}
+			}
+			label.show();
+		}
+		
+		//visualization
+		//clears the area where the next piece is shown (top)
+		public void clearNext() {
+			label.filledRectangleLL(0, ROWS+.9, COLS, 4.2, TLabel.DEFAULT_CLEAR_COLOR);
+			label.line(0, 0, 0, ROWS+5);
+			label.line(COLS, 0, COLS, ROWS+5);
+		}
+		
+
+		
+
+	}
+
+
+
+
+
+
 }
+
+
+
